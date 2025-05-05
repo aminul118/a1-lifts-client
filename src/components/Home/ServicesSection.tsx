69d2d47a522b1cca48d2d10ac92078fdeb666bed
Md Aminul Islam <mr.aminul118@gmail.com>
2025-05-05 11:04:38 +0600

    Services refine

--- src/components/Home/ServicesSection.tsx
+++ src/components/Home/ServicesSection.tsx
@@ -1,33 +1,6 @@
 import Image from "next/image";
-import { Flag, Wrench, Monitor } from "lucide-react";
 import Container from "../ui/Container";
-
-const services = [
-  {
-    title: "Installation",
-    description:
-      "We provide expert installation services ensuring safety, performance, and compliance with all standards.",
-    icon: <Flag className="text-white w-5 h-5" />,
-    iconBg: "bg-green-600",
-    image: "/assets/images/services/elevator-maintenance.png",
-  },
-  {
-    title: "Maintenance",
-    description:
-      "Keep your systems running smoothly with our preventive and corrective maintenance solutions.",
-    icon: <Wrench className="text-white w-5 h-5" />,
-    iconBg: "bg-green-600",
-    image: "/assets/images/services/servicing.jpg",
-  },
-  {
-    title: "Monitoring",
-    description:
-      "Real-time monitoring solutions to ensure optimal performance and rapid response to issues.",
-    icon: <Monitor className="text-white w-5 h-5" />,
-    iconBg: "bg-green-600",
-    image: "/assets/images/services/emergency-generators.jpg",
-  },
-];
+import services from "@/lib/constants/services";
 
 const ServicesSection = () => {
   return (
