39c06ba1cd3e5429c0c9a2e9522686165ee9202c
Md Aminul Islam <mr.aminul118@gmail.com>
2025-05-07 08:08:07 +0600

    Spare Parts data and image change

--- src/lib/constants/partsData.tsx
+++ src/lib/constants/partsData.tsx
@@ -6,44 +6,40 @@ const partsData = [
   },
   {
     id: 2,
-    product_name: "AVR",
-    product_img: "/assets/images/products/avr.png",
+    product_name: "ARD",
+    product_img: "/assets/images/products/ard.png",
   },
   {
     id: 3,
-    product_name: "AVR",
-    product_img: "/assets/images/products/avr.png",
+    product_name: "Controller",
+    product_img: "/assets/images/products/controller.png",
   },
   {
     id: 4,
-    product_name: "AVR",
-    product_img: "/assets/images/products/avr.png",
+    product_name: "Door Operator",
+    product_img: "/assets/images/products/door-operatorDoor.png",
   },
   {
     id: 5,
-    product_name: "AVR",
-    product_img: "/assets/images/products/avr.png",
+    product_name: "Door Hanger",
+    product_img: "/assets/images/products/hangerDoor.png",
   },
   {
     id: 6,
-    product_name: "AVR",
-    product_img: "/assets/images/products/avr.png",
+    product_name: "Door Inverter",
+    product_img: "/assets/images/products/door-inverterDoor.png",
   },
   {
     id: 7,
-    product_name: "AVR",
-    product_img: "/assets/images/products/avr.png",
+    product_name: "Gearless Traction Machine",
+    product_img: "/assets/images/products/gearless-traction.png",
   },
   {
     id: 8,
-    product_name: "AVR",
-    product_img: "/assets/images/products/avr.png",
+    product_name: "Rope Steel",
+    product_img: "/assets/images/products/ropeSteel.png",
   },
-  {
-    id: 9,
-    product_name: "AVR",
-    product_img: "/assets/images/products/avr.png",
-  },
+ 
 ];
 
 export default partsData;
