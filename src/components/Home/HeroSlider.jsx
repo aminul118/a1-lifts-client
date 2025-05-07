53731745cbe2053d019ba56c2af26c6e60012605
Md Aminul Islam <mr.aminul118@gmail.com>
2025-05-07 08:11:23 +0600

    Optional chaning added on hero slider

--- src/components/Home/HeroSlider.jsx
+++ src/components/Home/HeroSlider.jsx
@@ -27,24 +27,23 @@ const HeroSlider = () => {
       className="mySwiper"
     >
       {heroSliderData?.map((item) => (
-        <SwiperSlide key={item.id}>
+        <SwiperSlide key={item?.id}>
           <div
             className="relative w-full h-[350px] lg:h-[500px] 2xl:h-[750px] bg-cover bg-center"
-            style={{ backgroundImage: `url(${item.img})` }}
+            style={{ backgroundImage: `url(${item?.img})` }}
           >
             {/* Overlay content */}
             <div className="absolute inset-0 bg-black/50 bg-opacity-20 flex items-center">
               <div
-                className={`w-full max-w-3xl mx-auto  flex flex-col justify-center items-center text-center px-4 ${item.class}`}
+                className={`w-full max-w-3xl mx-auto  flex flex-col justify-center items-center text-center px-4 ${item?.class}`}
               >
                 <h2 className="text-2xl md:text-4xl font-bold mb-4">
                   {item.title}
                 </h2>
                 <p className="text-base md:text-lg mb-6 break-words">
-                  {item.description}
+                  {item?.description}
                 </p>
-
-                <Link href={item.url}>
+                <Link href={item?.url}>
                   <Button variant="outline"> {item?.button_text}</Button>
                 </Link>
               </div>
--- src/components/LiftServices/LiftServices.tsx
+++ src/components/LiftServices/LiftServices.tsx
@@ -6,7 +6,7 @@ import liftServices from "@/lib/constants/liftServices";
 const LiftServices = () => {
   return (
     <Container className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 md:px-12 py-10">
-      {liftServices.map((service, i) => (
+      {liftServices?.map((service, i) => (
         <div
           data-aos="fade-up"
           key={i}
@@ -14,18 +14,18 @@ const LiftServices = () => {
         >
           <div className="relative w-full h-60 2xl:h-72 overflow-hidden">
             <Image
-              src={service.image}
-              alt={service.service}
+              src={service?.image}
+              alt={service?.service}
               fill
               className="object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
             />
           </div>
           <div className="p-4">
             <h3 className="text-xl font-semibold mb-2 text-primary">
-              {service.service}
+              {service?.service}
             </h3>
             <div className="space-y-2 text-gray-600 dark:text-white/90">
-              {service.details.map((detail, idx) => (
+              {service?.details?.map((detail, idx) => (
                 <div key={idx} className="flex  gap-2">
                   <CheckCircle size={20} className="text-blue-600 shrink-0" />
                   <p className="leading-snug">{detail}</p>
