89fed539c9589f2579c846e96e389184e68f5c75
Md Aminul Islam <mr.aminul118@gmail.com>
2025-05-11 01:29:50 +0600

    Hero slader using Next Image component

--- src/components/Home/HeroSlider.jsx
+++ src/components/Home/HeroSlider.jsx
@@ -7,6 +7,7 @@ import "swiper/css/navigation";
 import "swiper/css/effect-fade";
 import { EffectFade, Pagination, Navigation, Autoplay } from "swiper/modules";
 import Link from "next/link";
+import Image from "next/image";
 import "@/styles/HeroSlider.css";
 import { Button } from "../ui/button";
 import heroSliderData from "@/lib/constants/heroSliderData";
@@ -31,18 +32,22 @@ const HeroSlider = () => {
     >
       {heroSliderData?.map((item) => (
         <SwiperSlide key={item?.id}>
-          <div
-            className="relative w-full h-[350px] lg:h-[500px] 2xl:h-[750px] bg-cover bg-center"
-            style={{ backgroundImage: `url(${item?.img})` }}
-          >
-            <div className="absolute inset-0 bg-black/50 bg-opacity-20 flex items-center">
+          <div className="relative w-full h-[350px] lg:h-[500px] 2xl:h-[750px] overflow-hidden">
+            <Image
+              src={item?.img}
+              alt={item?.title}
+              fill
+              className="object-cover object-center z-0"
+              priority
+            />
+            <div className="absolute inset-0 bg-black/50 z-10 flex items-center">
               <div
                 className={`w-full max-w-3xl mx-auto flex flex-col justify-center items-center text-center px-4 ${item?.class}`}
               >
-                <h2 className="text-2xl md:text-4xl font-bold mb-4">
+                <h2 className="text-2xl md:text-4xl font-bold mb-4 text-white">
                   {item.title}
                 </h2>
-                <p className="text-base md:text-lg mb-6 break-words">
+                <p className="text-base md:text-lg mb-6 text-white break-words">
                   {item?.description}
                 </p>
                 <Link href={item?.url}>
