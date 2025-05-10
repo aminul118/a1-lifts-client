b1e66084c1ca120f7f749c9ccecf5e5767da169a
Md Aminul Islam <mr.aminul118@gmail.com>
2025-05-11 01:56:23 +0600

    Section banner using next js Image component

--- src/components/ui/SectionBanner.tsx
+++ src/components/ui/SectionBanner.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Banner } from "@/lib/types/types";
 
 const SectionBanner = ({
@@ -8,19 +9,29 @@ const SectionBanner = ({
 }: Banner) => {
   return (
     <div
-      className={`${className} relative w-full h-64 lg:h-96 bg-cover bg-center  flex items-center justify-center `}
-      style={{
-        backgroundImage: backgroundImagePath
-          ? `url('${backgroundImagePath}')`
-          : "none",
-      }}
+      className={`${className} relative w-full h-64 lg:h-96 2xl:[450px] flex items-center justify-center overflow-hidden`}
     >
       {backgroundImagePath && (
-        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <Image
+          src={backgroundImagePath}
+          alt={`${heading} | Banner of a1 lifts`}
+          fill
+          className="object-cover"
+          priority
+        />
       )}
-      <div className="relative z-10 text-center max-w-2xl px-4">
-        <h2 className="text-3xl 2xl:text-5xl  font-bold mb-6 ">{heading}</h2>
-        {description && <p className="lg:text-lg">{description}</p>}
+
+      {/* Overlay */}
+      {backgroundImagePath && (
+        <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
+      )}
+
+      {/* Text content */}
+      <div className="relative z-20 text-center max-w-2xl px-4">
+        <h2 className="text-3xl 2xl:text-5xl font-bold mb-6 text-white">
+          {heading}
+        </h2>
+        {description && <p className="lg:text-lg text-white">{description}</p>}
       </div>
     </div>
   );
