862699ceafec2233b5760216d5ebd631bd7f844b
Md Aminul Islam <mr.aminul118@gmail.com>
2025-05-21 09:39:18 +0600

    Genarator products added

--- src/app/(root)/(products)/genarators/page.tsx
+++ src/app/(root)/(products)/genarators/page.tsx
@@ -1,10 +1,12 @@
-import ProductsCard from "@/components/Products/ProductCard";
+import { Card } from "@/components/ui/card";
+import Container from "@/components/ui/Container";
 import SectionBanner from "@/components/ui/SectionBanner";
+import genaratorData from "@/lib/constants/genaratorData";
 import { generateMetaTags } from "@/lib/genarateMetaTags";
 import { Metadata } from "next";
+import Image from "next/image";
 
 //>> SEO Start
-
 export const metadata: Metadata = generateMetaTags({
   title: "Generators - A1 Lifts and Engineering",
   description:
@@ -14,7 +16,6 @@ export const metadata: Metadata = generateMetaTags({
   image: "/assets/screenshots/hero.png",
   path: "generators",
 });
-
 //>> SEO END
 
 const GenaratorsPage = () => {
@@ -25,7 +26,64 @@ const GenaratorsPage = () => {
         className="text-white"
         backgroundImagePath="/assets/backgrounds/genarator.jpg"
       />
-      <ProductsCard category="genarator" />
+
+      <Container className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 py-10">
+        {genaratorData?.map((genarator) => {
+          const {
+            id,
+            product_name,
+            image,
+            Frequency,
+            Power,
+            rated_voltage,
+            warranty,
+          } = genarator;
+
+          return (
+            <Card
+              data-aos="fade-up"
+              key={id}
+              className="group block bg-white dark:bg-slate-800 overflow-hidden transition-transform py-0"
+            >
+              <div className="relative w-full h-60 2xl:h-72 overflow-hidden">
+                <Image
+                  src={image}
+                  alt={product_name}
+                  fill
+                  className="object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
+                />
+              </div>
+              <div className="p-4">
+                <h3 className="text-xl font-semibold mb-2 text-primary">
+                  {product_name}
+                </h3>
+                <div className="space-y-2 text-gray-600 dark:text-white/90">
+                  <div className="col-span-2 space-y-2">
+                    <div className="text-sm space-y-2">
+                      <p>
+                        <span className="font-semibold">Frequency: </span>
+                        {Frequency}
+                      </p>
+                      <p>
+                        <span className="font-semibold">Power: </span>
+                        {Power}
+                      </p>
+                      <p>
+                        <span className="font-semibold">Voltage: </span>
+                        {rated_voltage}
+                      </p>
+                      <p>
+                        <span className="font-semibold">Warranty: </span>
+                        {warranty}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Card>
+          );
+        })}
+      </Container>
     </div>
   );
 };
---
+++ src/lib/constants/genaratorData.tsx
@@ -0,0 +1,84 @@
+const genaratorData = [
+  {
+    id: 1,
+    product_name:
+      "Original Engine Container Diesel Generator Big Dg Generator 16KW To 1500KW",
+    image: "/assets/images/products/genarator/product1.webp",
+    rated_voltage: "400/230V, 110-480V, 220V / 380V, 110V / 240V, 380V",
+    warranty: "12 Months/1000 Hours",
+    Frequency: "50/60HZ Rated",
+    Power: "20KVA-1800KVA",
+  },
+  {
+    id: 2,
+    product_name:
+      "Diesel Genertor, Generator 250kw, Powered By Cummins Diesel Generator, Standby Use Power Diesel Generator 1000kw",
+    image: "/assets/images/products/genarator/product2.webp",
+    rated_voltage: "400/230V, 110-480V, 220V / 380V, 110V / 240V, 380V",
+    warranty: "12 Months/1000 Hours",
+    Frequency: "50/60HZ Rated",
+    Power: "100kva",
+  },
+  {
+    id: 3,
+    product_name:
+      "Diesel Genertor, Generator 250kw, Powered By Cummins Diesel Generator, Standby Use Power Diesel Generator 1000kw",
+    image: "/assets/images/products/genarator/product3.webp",
+    rated_voltage: "400/230V, 110-480V, 220V / 380V, 110V / 240V, 380V",
+    warranty: "12 Months/1000 Hours",
+    Frequency: "50/60HZ Rated",
+    Power: "20KVA-1800KVA",
+  },
+  {
+    id: 4,
+    product_name:
+      "Diesel Genertor, Generator 250kw, Powered By Cummins Diesel Generator, Standby Use Power Diesel Generator 1000kw",
+    image: "/assets/images/products/genarator/product4.webp",
+    rated_voltage: "400/230V, 110-480V, 220V / 380V, 110V / 240V, 380V",
+    warranty: "12 Months/1000 Hours",
+    Frequency: "50/60HZRated",
+    Power: "20KVA-1800KVA",
+  },
+  {
+    id: 5,
+    product_name:
+      "Diesel Genertor, Generator 250kw, Powered By Cummins Diesel Generator, Standby Use Power Diesel Generator 1000kw",
+    image: "/assets/images/products/genarator/product5.webp",
+    rated_voltage: "400/230V, 110-480V, 220V / 380V, 110V / 240V, 380V",
+    warranty: "12 Months/1000 Hours",
+    Frequency: "50/60HZRated",
+    Power: "20KVA-1800KVA",
+  },
+  {
+    id: 6,
+    product_name:
+      "Diesel Genertor, Generator 250kw, Powered By Cummins Diesel Generator, Standby Use Power Diesel Generator 1000kw",
+    image: "/assets/images/products/genarator/product6.webp",
+    rated_voltage: "400/230V, 110-480V, 220V / 380V, 110V / 240V, 380V",
+    warranty: "12 Months/1000 Hours",
+    Frequency: "50/60HZRated",
+    Power: "20KVA-1800KVA",
+  },
+  {
+    id: 7,
+    product_name:
+      "Diesel Genertor, Generator 250kw, Powered By Cummins Diesel Generator, Standby Use Power Diesel Generator 1000kw",
+    image: "/assets/images/products/genarator/product7.webp",
+    rated_voltage: "400/230V, 110-480V, 220V / 380V, 110V / 240V, 380V",
+    warranty: "12 Months/1000 Hours",
+    Frequency: "50/60HZRated",
+    Power: "20KVA-1800KVA",
+  },
+  {
+    id: 8,
+    product_name:
+      "Diesel Genertor, Generator 250kw, Powered By Cummins Diesel Generator, Standby Use Power Diesel Generator 1000kw",
+    image: "/assets/images/products/genarator/product8.webp",
+    rated_voltage: "400/230V, 110-480V, 220V / 380V, 110V / 240V, 380V",
+    warranty: "12 Months/1000 Hours",
+    Frequency: "50/60HZRated",
+    Power: "20KVA-1800KVA",
+  },
+];
+
+export default genaratorData;
