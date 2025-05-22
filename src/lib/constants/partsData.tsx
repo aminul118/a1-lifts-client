f02aa1f168e96b63200693b2983bf790817285ba
Md Aminul Islam <mr.aminul118@gmail.com>
2025-05-22 07:07:11 +0600

    substation parts added

--- src/lib/constants/partsData.tsx
+++ src/lib/constants/partsData.tsx
@@ -62,6 +62,68 @@ const partsData = [
       },
     ],
   },
+  {
+    category: "substations",
+    products: [
+      {
+        id: 1,
+        product_name: "Circuit Breaker",
+        product_img: "/assets/images/parts/substation/Circuit-Breaker.png",
+      },
+      {
+        id: 2,
+        product_name: "Dropout Fuse",
+        product_img: "/assets/images/parts/substation/Dropout-Fuse.png",
+      },
+      {
+        id: 3,
+        product_name: "Electrical Copper Busbar",
+        product_img:
+          "/assets/images/parts/substation/Electrical-Copper-Busbar.png",
+      },
+      {
+        id: 4,
+        product_name: "Three Phase Meter",
+        product_img: "/assets/images/parts/substation/Three-Phase-Meter.png",
+      },
+      {
+        id: 5,
+        product_name: "Single Phase Meter",
+        product_img: "/assets/images/parts/substation/Single-Phase-Meter.png",
+      },
+      {
+        id: 6,
+        product_name: "LTCT Meter",
+        product_img: "/assets/images/parts/substation/LTCT-Meter.png",
+      },
+      {
+        id: 7,
+        product_name: "Magnetic Contactor",
+        product_img: "/assets/images/parts/substation/Magnetic-Contactor.png",
+      },
+      {
+        id: 8,
+        product_name: "PFI Capacitor",
+        product_img: "/assets/images/parts/substation/PFI-Capacitor.png",
+      },
+      {
+        id: 9,
+        product_name: "HT Meter",
+        product_img: "/assets/images/parts/substation/HT-Meter.png",
+      },
+      {
+        id: 10,
+        product_name: "PIB Tape",
+        product_img: "/assets/images/parts/substation/PIB-Tape.png",
+      },
+      {
+        id: 11,
+        product_name: "Heatshrink Cable indoor Outdoor",
+        product_img:
+          "/assets/images/parts/substation/Heatshrink-Cable-indoor-Outdoor.png",
+      },
+    ],
+  },
 ];
 
 export default partsData;
